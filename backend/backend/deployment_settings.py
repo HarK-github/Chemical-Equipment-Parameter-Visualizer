25b2363a0bb3a39565b76e2f379b3590b28634cc
HarK <kandpalhar@gmail.com>
2025-11-22 21:51:39 +0530

    Update deployment_settings.py

--- backend/backend/deployment_settings.py
+++ backend/backend/deployment_settings.py
@@ -41,6 +41,14 @@ else:
             }
         }
     }
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.AllowAny",
+    ],
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ],
+}
 
 # =====================
 # Middleware
